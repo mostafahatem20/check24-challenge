21dcd7d829bd224bd605561de9c4f816959e80e0
mostafahatem20 <m.abdelrahman@finway.de>
2023-11-18 11:15:52 +0100

    add profiles

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { CraftsmenModule } from './craftsmen/craftsmen.module';
+import { ProfilesModule } from './profiles/profiles.module';
 
 @Module({
-  imports: [CraftsmenModule],
+  imports: [CraftsmenModule, ProfilesModule],
 })
 export class AppModule {}
