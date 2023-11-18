4235c6aff7695277ad9cea89208acfa3465fb822
mostafahatem20 <m.abdelrahman@finway.de>
2023-11-18 17:08:17 +0100

    remove unneeded files

--- packages/backend/src/postal-codes/postal-codes.service.ts
+++ packages/backend/src/postal-codes/postal-codes.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { CreatePostalCodeDto } from './dto/create-postal-code.dto';
-import { UpdatePostalCodeDto } from './dto/update-postal-code.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { PostalCode } from './entities/postal-code.entity';
@@ -8,26 +6,6 @@ import { PostalCode } from './entities/postal-code.entity';
 export class PostalCodesService {
   constructor(
     @InjectRepository(PostalCode)
-    private usersRepository: Repository<PostalCode>,
+    private readonly repository: Repository<PostalCode>,
   ) {}
-
-  create(createPostalCodeDto: CreatePostalCodeDto) {
-    return 'This action adds a new postalCode';
-  }
-
-  findAll() {
-    return `This action returns all postalCodes`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} postalCode`;
-  }
-
-  update(id: number, updatePostalCodeDto: UpdatePostalCodeDto) {
-    return `This action updates a #${id} postalCode`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} postalCode`;
-  }
 }
--- packages/backend/src/quality-factor-scores/quality-factor-scores.module.ts
+++ packages/backend/src/quality-factor-scores/quality-factor-scores.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { QualityFactorScore } from './entities/quality-factor-score.entity';
-import { QualityFactorScoreController } from './quality-factor-scores.controller';
 import { QualityFactorScoreService } from './quality-factor-scores.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([QualityFactorScore])],
-  controllers: [QualityFactorScoreController],
   providers: [QualityFactorScoreService],
   exports: [QualityFactorScoreService, TypeOrmModule],
 })
